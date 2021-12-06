00d71f2f1ee484d24cb3a1110a4d1bfff62e8f23
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-12-06 12:49:36 +0600

    Add files via upload

---
+++ math function use.py
@@ -0,0 +1,9 @@
+from math import *
+print("Maximun number is :",max(10,40))
+print("Miniumum number is :",min(10,40))
+print("Absulat value is :",abs(-5))
+print("Power is two number is :",pow(2,5))
+print("Sqt is :",sqrt(25))
+print("Round number is :" ,round(3.7))
+print("Floor number is :",floor(3.2))
+print("Ceil number is :",ceil(3.5))
